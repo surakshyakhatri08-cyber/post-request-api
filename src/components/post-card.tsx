a9500b3f2f545cb4c0778d3bf7cd5da2acf4a522
Surakshya Khatri <surakshyakhatri08@gmail.com>
2026-04-26 15:20:02 +0545

    Delete src/components/post-card.tsx
    Not Required

--- src/components/post-card.tsx
+++
@@ -1,8 +0,0 @@
-export default function PostCard({ title, body }: { title: string; body: string }) {
-  return (
-    <div className="p-4 bg-white shadow rounded-lg border border-gray-200">
-      <h2 className="font-bold text-blue-700 capitalize">{title}</h2>
-      <p className="text-gray-500 text-sm mt-2">{body.substring(0, 80)}...</p>
-    </div>
-  );
-}
